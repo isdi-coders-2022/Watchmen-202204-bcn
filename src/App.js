8fad45b47450ea53d5e61a7049606a381e759ee1
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-02 18:44:16 +0200

    HeaderComponent and StyleHeaderComponent added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import InfoComponent from "./components/InfoComponent/InfoComponent";
+import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
       <h1>WORK IN PROGRESS</h1>
-      <InfoComponent
-        titleText={"WELCOME TO OUR PAGE"}
-        paragraphText={"lorem ipsum dolot sic amet"}
-      />
     </div>
   );
 }
