b64c514c82cbf6ed53fe06a1984f12f37fe5eb56
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-02 16:25:40 +0200

    Adding Info Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import InfoComponent from "./components/InfoComponent/InfoComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>WORK IN PROGRESS</h1>
+      <InfoComponent
+        titleText={"WELCOME TO OUR PAGE"}
+        paragraphText={"lorem ipsum dolot sic amet"}
+      />
     </div>
   );
 }
