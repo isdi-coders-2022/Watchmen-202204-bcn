a545fb4c5b8b1a6d40c0946c9a5492d28dbdf733
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-02 17:39:42 +0200

    Adding fix for Data Layer in Readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import InfoComponent from "./components/InfoComponent/InfoComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>WORK IN PROGRESS</h1>
-      <InfoComponent
-        titleText={"WELCOME TO OUR PAGE"}
-        paragraphText={"lorem ipsum dolot sic amet"}
-      />
     </div>
   );
 }
