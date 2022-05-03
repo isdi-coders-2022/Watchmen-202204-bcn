f6b53e49b5f8c585b4986162b4c4d5c3223c9fd3
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-03 18:15:53 +0200

    Adding CollectionPage because it was running though another test

--- src/components/ListComponent/ListComponent.test.js
+++ src/components/ListComponent/ListComponent.test.js
@@ -1,10 +1,15 @@
 const { render, screen } = require("@testing-library/react");
+const { BrowserRouter } = require("react-router-dom");
 const { default: ListComponent } = require("./ListComponent");
 
 describe("Given a ListComponent Component", () => {
   describe("When it's call)", () => {
     test("Then it should render a ListComponent with 3 li item", () => {
-      render(<ListComponent />);
+      render(
+        <BrowserRouter>
+          <ListComponent />
+        </BrowserRouter>
+      );
 
       const expectedNumOfLi = 3;
 
