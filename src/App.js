28fa0a3a0a1831bee211d763dfac9b29b95d272a
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-03 13:02:53 +0200

    DetailPage added and Header removed from App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 
 function App() {
   return (
     <div className="App">
-      <HeaderComponent />
-      <h1>WORK IN PROGRESS</h1>
+      <h1>Working in progress</h1>
     </div>
   );
 }
