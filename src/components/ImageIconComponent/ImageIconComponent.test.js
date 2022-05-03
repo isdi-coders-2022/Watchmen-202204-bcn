e46b9db6e2b0592757345e9993374cca4c8e4ac7
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-03 13:39:44 +0200

    ImageIconComponent header render text tested

---
+++ src/components/ImageIconComponent/ImageIconComponent.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import ImageIconComponent from "./ImageIconComponent";
+
+describe("Given a InfoComponent component", () => {
+  describe("When it receives a text 'Add Item''", () => {
+    test("Then it should render the given text", () => {
+      const titleText = "Add Item";
+
+      render(<ImageIconComponent heading1={titleText}></ImageIconComponent>);
+      const textElement = screen.getByRole("heading", { name: titleText });
+
+      expect(textElement).toBeInTheDocument();
+    });
+  });
+});
