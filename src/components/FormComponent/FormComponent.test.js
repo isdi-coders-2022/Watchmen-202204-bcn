ef52958710b2184c8cabbf5c6d2fa9c37b60a3fc
victor90braz <victor.90braz@gmail.com>
2022-05-03 17:34:10 +0200

    Refactored FormComponent component add getByLabelText

--- src/components/FormComponent/FormComponent.test.js
+++ src/components/FormComponent/FormComponent.test.js
@@ -4,10 +4,9 @@ import FormComponent from "./FormComponent";
 describe("Given a FormComponent component", () => {
   describe("When it receives a nameLabel 'Title'", () => {
     test("Then it should render the given nameLabel", () => {
-      const nameLabel = "Title";
       render(<FormComponent />);
 
-      const textElement = screen.getByLabelText(nameLabel);
+      const textElement = screen.getByLabelText();
       expect(textElement).toBeInTheDocument();
     });
   });
