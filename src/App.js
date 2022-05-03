4650f5e583a14aaae92a51862476fa43c5053a21
Daniell Marafon <daniellmarafon@gmail.com>
2022-05-03 17:41:41 +0200

    Fixed hamburguer menu hidding behind elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import CollectionPage from "./pages/CollectionPage/CollectionPage";
 import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <CollectionPage></CollectionPage>
+      <HomePage />
     </div>
   );
 }
