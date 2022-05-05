8ff7797572b9221d3d4c945b5da0418987a9b649
Gerard Albajar <gerard.albajar@gmail.com>
2022-05-05 12:10:14 +0200

    DetailData added, DetailComponent and DetailPage updated

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,13 +1,17 @@
 import DetailComponent from "../../components/DetailComponent/DetailComponent";
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
 import ImageIconComponent from "../../components/ImageIconComponent/ImageIconComponent";
+import detailData from "../../dataTest/detailData";
 
 const DetailPage = () => {
   return (
     <>
       <HeaderComponent />
       <DetailComponent />
-      <ImageIconComponent image="/images/background.jpeg" />
+      <ImageIconComponent
+        image={detailData[0].artObject.webImage.url}
+        description={detailData[0].artObject.title}
+      />
     </>
   );
 };
